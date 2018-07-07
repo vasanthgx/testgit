6d42406c5bf3882d571bc13e9b73a770b1abb45e
Vasanth <vasanth1627@gmail.com>
2018-07-08 04:35:23 +0530

    first commit

---
+++ some.py
@@ -0,0 +1,7 @@
+rivers = ['ganges', 'narmada', 'cauvery', 'bramaputra']
+
+print('Printing the names of some Indian rivers ')
+print('------------------------------------------ \n\n\n')
+
+for riv in rivers:
+	print(riv.title())
